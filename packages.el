e9f43e2335e36536b2b5c626f1bc2d4ead174fcc
eeowaa <7460185+eeowaa@users.noreply.github.com>
2020-12-01 16:37:28 -0700

    Rearrange `package!` declarations

--- packages.el
+++ packages.el
@@ -8,8 +8,12 @@
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
-(package! counsel-spotify)
+
+;; UI
 (package! page-break-lines)
+
+;; Music
+(package! counsel-spotify)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
