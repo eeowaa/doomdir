403b3524652c9937d4d1e46e61511b7ab891b5ef
eeowaa <7460185+eeowaa@users.noreply.github.com>
2021-10-07 10:07:45 -0600

    Add `nov` (epub reader)

--- packages.el
+++ packages.el
@@ -23,6 +23,7 @@
 ;; :app
 (package! counsel-spotify)
 (package! noaa)
+(package! nov)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
