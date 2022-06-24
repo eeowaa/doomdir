6db3cd000362c2eaaa6b6537aa1f5507a19c4c26
eeowaa <7460185+eeowaa@users.noreply.github.com>
2022-06-24 12:14:08 -0600

    [WIP] Fixes for recent Doom CLI updates

--- cli.el
+++ cli.el
@@ -1,20 +1,22 @@
 ;;; DOOMDIR/cli.el -*- lexical-binding: t; -*-
 
-(defcligroup! "Demos"
-  "Demonstrations of basic Doom CLI functionality"
+(defgroup! "Demos"
+  :docs "Demonstrations of basic Doom CLI functionality"
   (defcli! echo-hello ()
     "Say \"hello world\" by calling out to echo."
+    ;; FIXME This now produces an error trace
     (throw 'exit (list "echo" "hello world")))
 
   (defcli! print-hello ()
     "Say \"hello world\" by evaluating print!."
     (print! "hello world")))
 
-(defcligroup! "Personal"
-  "My own custom Doom CLI commands"
+(defgroup! "Personal"
+  :docs "My own custom Doom CLI commands"
 
   (defcli! checkout-main ()
     "Checkout main branch for each repo managed by straight."
+    ;; FIXME This (probably) now produces an error trace
     (throw 'exit `(,(concat
                      (file-name-as-directory
                       (expand-file-name "bin" doom-private-dir))
--- early-init.el
+++ early-init.el
@@ -1,8 +1,2 @@
 (use-package-hook! persp-mode
   :pre-init (setq persp-keymap-prefix (kbd "C-c w")))
-
-(when noninteractive
-  (pushnew! doom-env-blacklist
-            "^AWS_ACCESS_KEY_ID$"
-            "^AWS_SECRET_ACCESS_KEY$"
-            "^AWS_SESSION_TOKEN$"))
