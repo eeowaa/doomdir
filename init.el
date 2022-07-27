6774010d373a3754957845edcbc26599621a8a14
eeowaa <7460185+eeowaa@users.noreply.github.com>
2022-07-26 18:55:46 -0600

    Add tree-sitter module and enable it for python

--- init.el
+++ init.el
@@ -112,7 +112,7 @@
        ;;taskrunner        ; taskrunner for all your projects
        terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
-       ;;tree-sitter       ; syntax and parsing, sitting in a tree...
+       tree-sitter       ; syntax and parsing, sitting in a tree...
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
@@ -167,7 +167,7 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +lsp +pyright)
+       (python +lsp +tree-sitter +pyright)
                          ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        (racket +lsp +xp) ; a DSL for DSLs
