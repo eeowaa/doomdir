0a46117b6577312874741c76cb8e342e172175c9
eeowaa <7460185+eeowaa@users.noreply.github.com>
2022-08-08 13:14:01 -0600

    Add support for presentations from Org mode

--- init.el
+++ init.el
@@ -162,7 +162,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +dragndrop +gnuplot +jupyter +noter +pandoc +pomodoro +roam2)
+       (org +dragndrop +gnuplot +jupyter +noter +pandoc +pomodoro +present +roam2)
                          ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
