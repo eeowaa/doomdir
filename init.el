da48350c510952b31eddf923264444cb71d0e3ab
eeowaa <7460185+eeowaa@users.noreply.github.com>
2022-09-22 14:39:42 -0600

    Add additional modules

--- init.el
+++ init.el
@@ -66,7 +66,7 @@
        ;;(format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
-       ;;multiple-cursors  ; editing in many places at once
+       multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
@@ -162,7 +162,7 @@
        (lua +lsp)          ; one-based indices? one-based indices
        (markdown +grip)  ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       ;;nix               ; I hereby declare "nix geht mehr!"
+       nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org +dragndrop +gnuplot +jupyter +noter +pandoc +pomodoro +present +roam2)
                          ; organize your plain life in plain text
