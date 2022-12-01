e96586e4d546e73c8db6e07b21671587af63c115
eeowaa <7460185+eeowaa@users.noreply.github.com>
2022-12-01 11:13:20 -0700

    Move AWS man page config to :emacs module

--- config.el
+++ config.el
@@ -510,49 +510,6 @@ _SPC_: Play/Pause    _l_: Playlist    _s_: By name     _o_: Application
       :desc "Raise popup"
       "^" #'+popup/raise)
 
-;; Allow any letter to be used a manual section for Man (AWS CLI uses "a")
-(setq Man-section-regexp "[a-zA-Z0-9+]+")
-
-;; Consider "AVAILABLE.*" page sections to be "SEE ALSO"
-(setq Man-see-also-regexp
-      (format "\\(%s\\)"
-              (string-join '("SEE ALSO"
-                             "VOIR AUSSI"
-                             "SIEHE AUCH"
-                             "VÉASE TAMBIÉN"
-                             "VEJA TAMBÉM"
-                             "VEDERE ANCHE"
-                             "ZOBACZ TAKŻE"
-                             "İLGİLİ BELGELER"
-                             "参照"
-                             "参见 SEE ALSO"
-                             "參見 SEE ALSO"
-                             "AVAILABLE.*") ;; For AWS CLI man pages
-                           "\\|")))
-
-;; Allow buttons to be properly overlayed on AWS CLI man page references
-(after! man
-  (setq
-   Man-reference-regexp
-   (concat
-    ;; Ignore bullet points
-    "\\(?:^\\.IP \\\\(bu 2\\n\\|o \\)?"
-    ;; This is the <name> part
-    "\\(" Man-name-regexp
-         "\\("
-              ;; This allow line-continuations for long man page names
-              ;;
-              ;; SEE ALSO
-              ;;     foo(1), bar(1), line-
-              ;;     continuation(1)
-              ;;
-              "\\([-‐]\n\\)?"
-              "[ \t]+" Man-name-regexp
-         "\\)*"
-    "\\)"
-    ;; This is the (<section>) part
-    "[ \t]*(\\(" Man-section-regexp "\\))")))
-
 (after! imenu-list
   (set-popup-rule! "^\\*Ilist\\*"
     :side 'right :size 35 :modeline "Ilist")
@@ -849,6 +806,49 @@ deleting the final newline before inserting the \")))\"."
             "sudo:root@" host
             ":" (or (file-remote-p file 'localname)
                     file))))
+
+;; Allow any letter to be used a manual section for Man (AWS CLI uses "a")
+(setq Man-section-regexp "[a-zA-Z0-9+]+")
+
+;; Consider "AVAILABLE.*" page sections to be "SEE ALSO"
+(setq Man-see-also-regexp
+      (format "\\(%s\\)"
+              (string-join '("SEE ALSO"
+                             "VOIR AUSSI"
+                             "SIEHE AUCH"
+                             "VÉASE TAMBIÉN"
+                             "VEJA TAMBÉM"
+                             "VEDERE ANCHE"
+                             "ZOBACZ TAKŻE"
+                             "İLGİLİ BELGELER"
+                             "参照"
+                             "参见 SEE ALSO"
+                             "參見 SEE ALSO"
+                             "AVAILABLE.*") ;; For AWS CLI man pages
+                           "\\|")))
+
+;; Allow buttons to be properly overlayed on AWS CLI man page references
+(after! man
+  (setq
+   Man-reference-regexp
+   (concat
+    ;; Ignore bullet points
+    "\\(?:^\\.IP \\\\(bu 2\\n\\|o \\)?"
+    ;; This is the <name> part
+    "\\(" Man-name-regexp
+         "\\("
+              ;; This allow line-continuations for long man page names
+              ;;
+              ;; SEE ALSO
+              ;;     foo(1), bar(1), line-
+              ;;     continuation(1)
+              ;;
+              "\\([-‐]\n\\)?"
+              "[ \t]+" Man-name-regexp
+         "\\)*"
+    "\\)"
+    ;; This is the (<section>) part
+    "[ \t]*(\\(" Man-section-regexp "\\))")))
 
 (setq confirm-kill-processes nil)
 
