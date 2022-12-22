2e94687ff60ae309550a5365fd5fa5e04840409b
eeowaa <7460185+eeowaa@users.noreply.github.com>
2022-12-22 02:02:30 -0700

    Package definitions and configurations for K8s

--- modules/tools/kubernetes/config.el
+++ modules/tools/kubernetes/config.el
@@ -3,31 +3,74 @@
 
 ;;; Kubernetes
 
-(use-package! kubedoc)
+(use-package! kubedoc
+  :defer t)
+
 (use-package! k8s-mode
-  :magic
+  :defer t
+  :preface
+  (defvar +kubernetes-k8s-mode-match-files
+    '("\\.ya?ml$" (not . "/templates/\\|\\(?:chart\\.ya?ml$\\)"))
+    "List of file name regexps to check for `k8s-mode' activation.
+Each element looks like REGEXP or (not . REGEXP), respectively
+describing positive and negative regexp patterns on file names.
+Elements in other forms are silently ignored. The regexps are matched
+case-insensitively.
 
-  ("\\`\\(?:.*\n\\)\\{0,10\\}apiVersion:" . k8s-mode)
+If a newly-opened file-visiting buffer has any positive regexp
+matches against its file name, the negative regexps are checked.
+If there are no negative matches, `+kubernetes--k8s-mode-match-fn'
+will proceed with its checks against the buffer content.")
 
+  (defvar +kubernetes-k8s-mode-match-lines 10)
+  (defvar +kubernetes-k8s-mode-match-regexp "^apiVersion:"
+    "File content regexp to check for `k8s-mode' activation.
+If a new file-visiting buffer meets the conditions imposed by
+`+kubernetes-k8s-mode-match-files', the first `+kubernetes-k8s-mode-match-lines'
+lines of the buffer are checked against this regexp. If there is a match,
+`k8s-mode' is activated in the buffer.")
+
+  (defun +kubernetes--k8s-mode-search-lines (line-count)
+    (goto-char (bol))
+    (and (cl-plusp line-count)
+         (or (looking-at-p +kubernetes-k8s-mode-match-regexp)
+             (and (line-move 1 t)
+                  (+kubernetes--k8s-mode-search-lines (1- line-count))))))
+
+  (defun +kubernetes--k8s-mode-match-fn ()
+    (let* ((file-name (buffer-file-name))
+           (file-name-string-match-p
+            (lambda (re)
+              (let ((case-fold-search t))
+                (string-match-p re file-name))))
+           positives negatives)
+      (dolist (element +kubernetes-k8s-mode-match-files)
+        (cond
+         ((stringp element)
+          (push element positives))
+         ((and (eq 'not (car-safe element))
+               (stringp (cdr element)))
+          (push (cdr element) negatives))))
+      (and
+       (cl-some file-name-string-match-p positives)
+       (cl-notany file-name-string-match-p negatives)
+       (save-excursion
+         (goto-char 1)
+         (+kubernetes--k8s-mode-search-lines +kubernetes-k8s-mode-match-lines)))))
   :init
-
-  ;;; Kubernetes: lsp-mode
-
+  (add-to-list 'magic-mode-alist '(+kubernetes--k8s-mode-match-fn . k8s-mode))
   (when (modulep! +lsp)
-    (after! lsp-mode
-      (add-hook! 'k8s-mode-local-vars-hook :append
-        (list
-         ;; NOTE: Doom's `lsp!' does not work well here
-         `(#'lsp
-           ,(defun my/k8s-mode-set-yaml-schema ()
-              "Enable the built-in Kubernetes YAML schema."
-              (lsp-yaml-set-buffer-schema
-               (alist-get 'url lsp-yaml--built-in-kubernetes-schema)))))))))
+    (add-hook! 'k8s-mode-local-vars-hook :append
+      (defun +kubernetes--k8s-mode-lsp-init ()
+        "Start the YAML LSP server using the built-in Kubernetes schema."
+        (lsp) ; Do not wait for the buffer to be visible.
+        (lsp-yaml-set-buffer-schema
+         (alist-get 'url lsp-yaml--built-in-kubernetes-schema))))))
 
 
 ;;; Helm
 
-(load! "package/kubernetes-helm-mode.el")
+(require 'kubernetes-helm-mode)
 (pushnew! auto-mode-alist
           '("/templates/.+\\.\\(?:ya?ml\\|tpl\\)\\'" . kubernetes-helm-mode))
 
@@ -41,7 +84,7 @@
 (when (and (modulep! +lsp)
            (modulep! :tools lsp)
            (not (modulep! :tools lsp +eglot)))
-  (load! "staging/lsp-kubernetes-helm.el")
+  (require 'lsp-kubernetes-helm)
   (add-hook 'kubernetes-helm-mode-hook #'lsp! 0 t))
 
 ;;; Helm: tree-sitter
@@ -88,17 +131,17 @@
         (define_action    . ts-fold-range-seq)
         (block_action     . ts-fold-range-seq)
         (with_action      . ts-fold-range-seq)))
-    (customize-set-variable 'ts-fold-range-alist
-                            (cons `(kubernetes-helm-mode . ,(ts-fold-parsers-gotmpl))
-                                  (assq-delete-all 'kubernetes-helm-mode ts-fold-range-alist)))
+    (setq! ts-fold-range-alist
+           (cons `(kubernetes-helm-mode . ,(ts-fold-parsers-gotmpl))
+                 (assq-delete-all 'kubernetes-helm-mode ts-fold-range-alist)))
     (defun ts-fold-summary-gotmpl (doc-str)
       "Extract summary from DOC-STR in Go template block."
       (let ((first-line (nth 0 (split-string doc-str "\n"))))
         (string-match "\\`{-? *\\(.*?\\)\\(?: *-?}}\\)? *\\'" first-line)
         (match-string 1 first-line)))
-    (customize-set-variable 'ts-fold-summary-parsers-alist
-                            (cons `(kubernetes-helm-mode . ts-fold-summary-gotmpl)
-                                  ts-fold-summary-parsers-alist)))
+    (setq! ts-fold-summary-parsers-alist
+           (cons `(kubernetes-helm-mode . ts-fold-summary-gotmpl)
+                 ts-fold-summary-parsers-alist)))
 
   ;;; Helm: evil-textobj-tree-sitter
 
--- modules/tools/kubernetes/packages.el
+++ modules/tools/kubernetes/packages.el
@@ -1,8 +1,22 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/kubernetes/packages.el
 
-(package! k8s-mode)
 (package! kubedoc)
 
-;(package! go-template-mode
-;  :recipe (:host nil :url "https://gist.github.com/eeowaa/3c18e0d7ad06ef8032e07ee3229f96e3"))
+(when (package! k8s-mode)
+  (package! kubernetes-helm-mode :recipe
+    (:local-repo "lisp"
+     :files ("kubernetes-helm-mode.el")
+     :build (:not compile))))
+
+(when (and (modulep! :tools lsp)
+           (not (modulep! :tools lsp +eglot))
+           (modulep! +lsp))
+  (package! lsp-kubernetes-helm :recipe
+    (:local-repo "lisp"
+     :files ("lsp-kubernetes-helm.el")
+     :build (:not compile))))
+
+(package! go-template-mode :recipe
+  (:host nil
+   :repo "https://gist.github.com/eeowaa/3c18e0d7ad06ef8032e07ee3229f96e3"))
