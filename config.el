54574c3055d5b26d79997f873dd9832d08541736
eeowaa <7460185+eeowaa@users.noreply.github.com>
2023-01-18 11:55:42 -0700

    Do not dim the background of buffers during window selection

--- config.el
+++ config.el
@@ -606,6 +606,8 @@ _SPC_: Play/Pause    _l_: Playlist    _s_: By name     _o_: Application
   (when initial-window-system
     (setq aw-scope 'visible)))
 
+(setq aw-background nil)
+
 (map! :leader
       (:when (modulep! :ui workspaces)
        (:prefix-map ("TAB" . "workspace")
