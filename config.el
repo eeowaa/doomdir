3bbca4f07ad8916aae550c22b0ad4d0cd4f0e39c
eeowaa <7460185+eeowaa@users.noreply.github.com>
2023-02-02 11:46:34 -0700

    Add treemacs icon override for .project files

--- config.el
+++ config.el
@@ -672,7 +672,7 @@ _SPC_: Play/Pause    _l_: Playlist    _s_: By name     _o_: Application
   '(("org" . ("org_archive"))
     ("sh" . ("bat"))
     ("txt" . ("Pipfile"))
-    ("json" . ("Pipfile.lock"))) ;; "configuration" icon
+    ("json" . ("Pipfile.lock" "project"))) ;; "configuration" icon
   "Alist of file extension mappings for Treemacs icons.
 
 The `car' of each element is a file extension with a desirable
