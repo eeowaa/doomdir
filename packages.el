261f66df72f527c126729ce5328616bffff96aff
eeowaa <7460185+eeowaa@users.noreply.github.com>
2023-02-16 14:31:10 -0700

    Fix bug caused by upstream changes to magit-related packages

--- packages.el
+++ packages.el
@@ -48,6 +48,9 @@
 ;; If this causes issues, try `(unpin! (:tools lsp))' instead
 (unpin! lsp-mode)
 
+(package! transient :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440")
+(package! with-editor :pin "bbc60f68ac190f02da8a100b6fb67cf1c27c53ab")
+
 (package! magit-gitflow :disable t)
 
 (unpin! tree-sitter-langs)
