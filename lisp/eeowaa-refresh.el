1bc612b14259d90cc6c0a7f45bac4d4f5853fcc2
eeowaa <7460185+eeowaa@users.noreply.github.com>
2023-02-20 14:53:59 -0700

    Minimal vdiff config

--- lisp/eeowaa-refresh.el
+++ lisp/eeowaa-refresh.el
@@ -1,14 +1,20 @@
 ;;; eeowaa-refresh.el -*- lexical-binding: t; -*-
 
+(defvar-local eeowaa-refresh-force nil
+  "Non-nil means to always call `revert-buffer' in `eeowaa-refresh-buffer'.")
+
 (defun eeowaa-refresh-buffer (&optional buffer-or-name)
   "Refresh non-file-visiting buffer contents with `revert-buffer'.
-BUFFER-OR-NAME defaults to the current buffer."
+BUFFER-OR-NAME defaults to the current buffer. The behavior of
+this function can be modified via the buffer-local variable
+`eeowaa-refresh-force'."
   (interactive)
   (with-current-buffer (or buffer-or-name (current-buffer))
-    (unless buffer-file-name
-      (if (local-variable-p 'revert-buffer-function)
-          (revert-buffer)
-        (message "No buffer-local `revert-buffer-function'")))))
+    (cond
+     (eeowaa-refresh-force (revert-buffer))
+     (buffer-file-name (prog1 nil (message "Refusing to revert file-visiting buffer.")))
+     ((local-variable-p 'revert-buffer-function) (revert-buffer))
+     (t (prog1 nil (message "No buffer-local `revert-buffer-function'"))))))
 
 (defun eeowaa-refresh-display ()
   "Redraw the display."
