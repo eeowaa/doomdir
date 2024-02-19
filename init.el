40478e1d383c1fc88c338dea8eb6cb00d12a480e
eeowaa <7460185+eeowaa@users.noreply.github.com>
2024-02-19 12:37:00 -0700

    Apply changes to init.example.el to init.el

--- init.el
+++ init.el
@@ -4,8 +4,8 @@
 ;; in. Remember to run 'doom sync' after modifying it!
 
 ;; NOTE Press 'SPC h d h' (or 'C-h d h' for non-vim users) to access Doom's
-;;      documentation. There you'll find a "Module Index" link where you'll find
-;;      a comprehensive list of Doom's modules and what flags they support.
+;;      documentation. There you'll find a link to Doom's Module Index where all
+;;      of our modules are listed, including what flags they support.
 
 ;; NOTE Move your cursor over a module's name (or its flags) and press 'K' (or
 ;;      'C-c c k' for non-vim users) to view its documentation. This works on
@@ -100,6 +100,7 @@
        ;;biblio            ; Writes a PhD for you (citation needed)
        bitwarden           ; XXX user module
        ;;cloudformation  ; TODO XXX user module
+       ;;collab            ; buffers with friends
        (debugger +lsp)   ; FIXME stepping through code, to help you add bugs
        direnv
        (docker +lsp)
@@ -126,14 +127,14 @@
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
-       (:if IS-MAC macos)  ; improve compatibility with macOS
+       (:if (featurep :system 'macos) macos) ; improve compatibility with macOS
        ;;tty               ; improve the terminal Emacs experience
 
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
        ;; NOTE `+eglot' adds commands for displaying inheritance type hierarchies
-       (cc +lsp +tree-sitter) ; C/C++/Obj-C madness
+       (cc +lsp +tree-sitter) ; C > C++ == 1
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
@@ -160,7 +161,7 @@
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
        (json +lsp +tree-sitter) ; At least it ain't XML
-       ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
+       ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
        (javascript +lsp +tree-sitter) ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
