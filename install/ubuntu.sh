5c17a63a619101ba8bfbca233436d2e0f1fe3fb0
eeowaa <7460185+eeowaa@users.noreply.github.com>
2026-07-04 12:34:06 -0600

    (wip) Add missing prerequisite blocks for Ubuntu

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -12,6 +12,50 @@ package updates may upgrade Emacs, resulting in:
 *** Use caution when performing system updates! ***
 EOF
 
+# Determine the WSL version (0 means not on WSL)
+case `uname -r` in
+*-WSL2)      WSL_VERSION=2 ;;
+*-Microsoft) WSL_VERSION=1 ;;
+*)           WSL_VERSION=0 ;;
+esac
+
+# TODO: Obtain Emacs source code corresponding to installed DPKG
+
+# Install Emacs info manuals
+sudo apt-get -y install emacs-common-non-dfsg
+
+# Install ripgrep and fd (Doom prerequisites)
+sudo apt-get -y install ripgrep fd-find
+mkdir -p ~/.local/bin && ln -sf /usr/bin/fdfind ~/.local/bin/fd
+
+if [ $WSL_VERSION -eq 1 ]
+then
+    # FIXME: Support NodeJS on WSL 1 (requires configuration hacks)
+    echo >&2 'ERROR: NodeJS configuration is unimplemented on WSL 1'
+    exit 1
+else
+    # Install NVM
+    curl -fsSLo- https://raw.githubusercontent.com/nvm-sh/nvm/HEAD/install.sh \
+        | env PROFILE=/dev/null bash
+
+    # Install NodeJS and NPM through NVM
+    nvm install node
+fi
+
+# Install pipx to install Python packages
+sudo apt-get -y install pipx
+
+# Install the gh-release-dl script to install binary assets from GitHub releases
+sudo apt-get -y install jq
+which gh-release-dl >/dev/null 2>&1 || {
+    # TODO: Install to a more "permanent" directory used by Doom Emacs
+    test -f "${TMPDIR:-/tmp}/gh-release-dl" || {
+        test -d "${TMPDIR:-/tmp}" || mkdir -p "${TMPDIR:-/tmp}"
+        curl -fsSLo "${TMPDIR:-/tmp}/gh-release-dl" https://raw.githubusercontent.com/eeowaa/stow-dotfiles/refs/heads/main/utils/.local/bin/gh-release-dl
+    }
+    chmod +x "${TMPDIR:-/tmp}/gh-release-dl"
+    alias gh-release-dl="${TMPDIR:-/tmp}/gh-release-dl"
+}
 
 # Install prerequisites for `ui/doom` module
 sudo apt-get -y install fonts-terminus
